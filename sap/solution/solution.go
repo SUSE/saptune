7da89eaafee524cd9d424d2ded487fe9684bb49b
AngelaBriel <abriel@suse.com>
2018-10-25 13:32:54 +0200

    read solution definition from file /usr/share/saptune/solution instead of static coding inside of saptune.

--- sap/solution/solution.go
+++ sap/solution/solution.go
@@ -6,10 +6,15 @@ A system can be tuned for more than one solutions at a time.
 package solution
 
 import (
+	"github.com/SUSE/saptune/system"
+	"github.com/SUSE/saptune/txtparser"
+	"log"
 	"sort"
+	"strings"
 )
 
 const (
+	SolutionSheet         = "/usr/share/saptune/solutions"
 	ArchX86     = "amd64"   // ArchX86 is the GOARCH value for x86 platform.
 	ArchPPC64LE = "ppc64le" // ArchPPC64LE is the GOARCH for 64-bit PowerPC little endian platform.
 	ArchX86_PC     = "amd64_PC"   // ArchX86 is the GOARCH value for x86 platform. _PC indicates PageCache is available
@@ -18,41 +23,68 @@ const (
 
 type Solution []string // Solution is identified by set of note numbers.
 
-var AllSolutions = map[string]map[string]Solution{
-	ArchX86: {
-		"BOBJ":             {"941735", "1771258", "1980196", "1984787", "SAP_BOBJ", "2534844"},
-		"SAP-ASE":          {"941735", "1771258", "1980196", "1410736", "1984787", "1680803", "2534844"},
-		"NETWEAVER":        {"941735", "1771258", "1980196", "1984787", "2534844"},
-		"MAXDB":            {"941735", "1771258", "1980196", "1984787", "2534844"},
-		"HANA":             {"941735", "1771258", "1980196", "1984787", "2205917", "2534844", "2382421"},
-		"S4HANA-APPSERVER": {"941735", "1771258", "1980196", "1984787", "2534844", "2382421"},            // identical to Netweaver
-		"S4HANA-DBSERVER":  {"941735", "1771258", "1980196", "1984787", "2205917", "2534844", "2382421"}, // identical to HANA
-	},
-	ArchPPC64LE: {
-		"NETWEAVER":        {"941735", "1771258", "1980196", "1984787", "2534844"},
-		"MAXDB":            {"941735", "1771258", "1980196", "1984787", "2534844"},
-		"HANA":             {"941735", "1771258", "1980196", "1984787", "2205917", "2534844", "2382421"},
-		"S4HANA-APPSERVER": {"941735", "1771258", "1980196", "1984787", "2534844", "2382421"},            // identical to Netweaver
-		"S4HANA-DBSERVER":  {"941735", "1771258", "1980196", "1984787", "2205917", "2534844", "2382421"}, // identical to HANA
+// Architecture VS solution ID VS note numbers
+// AllSolutions = map[string]map[string]Solution
+var AllSolutions = GetSolutionDefintion()
 
-	},
-	ArchX86_PC: {
-		"BOBJ":             {"941735", "1771258", "1980196", "1984787", "1557506", "SAP_BOBJ", "2534844"},
-		"SAP-ASE":          {"941735", "1771258", "1980196", "1410736", "1984787", "1557506", "1680803", "2534844"},
-		"NETWEAVER":        {"941735", "1771258", "1980196", "1984787", "2534844", "1557506"},
-		"MAXDB":            {"941735", "1771258", "1980196", "1984787", "2534844", "1557506"},
-		"HANA":             {"941735", "1771258", "1980196", "1984787", "1557506", "2205917", "2534844", "2382421"},
-		"S4HANA-APPSERVER": {"941735", "1771258", "1980196", "1984787", "1557506", "2534844", "2382421"},            // identical to Netweaver
-		"S4HANA-DBSERVER":  {"941735", "1771258", "1980196", "1984787", "1557506", "2205917", "2534844", "2382421"}, // identical to HANA
-	},
-	ArchPPC64LE_PC: {
-		"NETWEAVER":        {"941735", "1771258", "1980196", "1984787", "2534844", "1557506"},
-		"MAXDB":            {"941735", "1771258", "1980196", "1984787", "2534844", "1557506"},
-		"HANA":             {"941735", "1771258", "1980196", "1984787", "1557506", "2205917", "2534844", "2382421"},
-		"S4HANA-APPSERVER": {"941735", "1771258", "1980196", "1984787", "1557506", "2534844", "2382421"},            // identical to Netweaver
-		"S4HANA-DBSERVER":  {"941735", "1771258", "1980196", "1984787", "1557506", "2205917", "2534844", "2382421"}, // identical to HANA
-	},
-} // Architecture VS solution ID VS note numbers
+// read solution definition from file
+// build same structure for AllSolutions as before
+// can be simplyfied later
+func GetSolutionDefintion() (map[string]map[string]Solution) {
+	sols := make(map[string]map[string]Solution)
+	sol  := make(map[string]Solution)
+	currentArch := ""
+	content, err := txtparser.ParseINIFile(SolutionSheet, false)
+	if err != nil {
+		log.Printf("Failed to read solution definition from file '%s'", SolutionSheet)
+		return sols
+	}
+
+	for _, param := range content.AllValues {
+		if param.Section == "reminder" {
+			continue
+		}
+		if param.Section != currentArch {
+			// start a new arch
+			if currentArch != "" {
+				// save previous arch settings
+				switch currentArch {
+				case "ArchPPC64LE":
+					if system.IsPagecacheAvailable() {
+						sols[ArchPPC64LE_PC] = sol
+					}
+					sols[ArchPPC64LE] = sol
+				case "ArchX86":
+					if system.IsPagecacheAvailable() {
+						sols[ArchX86_PC] = sol
+					}
+					sols[ArchX86] = sol
+				}
+			}
+			currentArch = param.Section
+			sol = make(map[string]Solution)
+		}
+
+		sol[param.Key] = strings.Split(param.Value,"\t")
+		if system.IsPagecacheAvailable() {
+			//add 1557506 (pagecache note) to list of notes
+			sol[param.Key] = append(sol[param.Key], "1557506")
+		}
+	}
+	switch currentArch {
+	case "ArchPPC64LE":
+		if system.IsPagecacheAvailable() {
+			sols[ArchPPC64LE_PC] = sol
+		}
+		sols[ArchPPC64LE] = sol
+	case "ArchX86":
+		if system.IsPagecacheAvailable() {
+			sols[ArchX86_PC] = sol
+		}
+		sols[ArchX86] = sol
+	}
+	return sols
+}
 
 // Return all solution names, sorted alphabetically.
 func GetSortedSolutionNames(archName string) (ret []string) {
