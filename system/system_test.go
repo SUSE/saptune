ec0004760fb9a9db72cbc9f3abaafbff060b1a5c
AngelaBriel <abriel@suse.com>
2019-01-10 12:03:20 +0100

    remove new line from println arg list of main.go to support newer go versions. (bsc#1120741) change 'if var1 == string1 || var1 == string2 || ...' to 'switch' to support the newer go compiler version

---
+++ system/system_test.go
@@ -0,0 +1,32 @@
+package system
+
+import (
+	"testing"
+)
+
+
+func TestIsUserRoot(t *testing.T) {
+	if !IsUserRoot() {
+		t.Log("the test requires root access")
+	}
+}
+
+func TestGetOsName(t *testing.T) {
+	actualVal := GetOsName()
+	if actualVal != "SLES" {
+		t.Logf("OS is '%s' and not 'SLES'\n", actualVal)
+	}
+	if actualVal == "" {
+		t.Logf("empty value returned for the os Name")
+	}
+}
+
+func TestGetOsVers(t *testing.T) {
+	actualVal := GetOsVers()
+	switch actualVal {
+	case "12", "12-SP1", "12-SP2", "12-SP3", "12-SP4", "15", "15-SP1":
+		t.Logf("expected OS version '%s' found\n", actualVal)
+	default:
+		t.Logf("unexpected OS version '%s'\n", actualVal)
+	}
+}
