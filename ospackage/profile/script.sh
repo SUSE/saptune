738a04b3679b34e6862492fa1befff5c3c1a4d20
AngelaBriel <abriel@suse.com>
2019-01-28 13:50:02 +0100

    support 'saptune note revert <id>' and 'saptune solution revert <sol>' again. add parameter based saved state files to support proper revert functionality change company name in copyright lines

--- ospackage/profile/script.sh
+++ ospackage/profile/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2016 SUSE LINUX GmbH, Nuernberg, Germany.
+# Copyright (c) 2016 SUSE LLC.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
