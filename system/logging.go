4f3581288b79617060314a7b3edf521b55f040c1
AngelaBriel <abriel@suse.com>
2024-01-26 18:49:35 +0100

    fix timestamp in log messages of saptune

--- system/logging.go
+++ system/logging.go
@@ -17,6 +17,7 @@ var errorLogger *log.Logger   // Error logger
 var warningLogger *log.Logger // Warning logger
 var verboseSwitch string      // Switch verbose mode on or off
 var errorSwitch string        // Switch error mode on or off
+var severDebugFormat = "DEBUG    "
 var severNoticeFormat = "NOTICE   "
 var severInfoFormat = "INFO     "
 var severWarnFormat = "WARNING  "
@@ -24,19 +25,26 @@ var severErrorFormat = "ERROR    "
 var logpidFormat = fmt.Sprintf("saptune[%v] ", os.Getpid()) // format to add pid of current saptune process to the log message
 var debugSwitch = os.Getenv("SAPTUNE_DEBUG")                // Switch Debug on or off
 
-// DebugLog sents text to the debugLogger and stderr
+//define log format
+func logTimeFormat() string {
+	return time.Now().Format("2006-01-02 15:04:05.000 ")
+}
+	
+// DebugLog sends text to the debugLogger and stderr
 func DebugLog(txt string, stuff ...interface{}) {
 	if debugSwitch == "on" {
 		if debugLogger != nil {
+			debugLogger.SetPrefix(logTimeFormat()+severDebugFormat+logpidFormat)
 			debugLogger.Printf(CalledFrom()+txt+"\n", stuff...)
 		}
 		fmt.Fprintf(os.Stderr, "DEBUG: "+txt+"\n", stuff...)
 	}
 }
 
-// NoticeLog sents text to the noticeLogger and stdout
+// NoticeLog sends text to the noticeLogger and stdout
 func NoticeLog(txt string, stuff ...interface{}) {
 	if noticeLogger != nil {
+		noticeLogger.SetPrefix(logTimeFormat()+severNoticeFormat+logpidFormat)
 		noticeLogger.Printf(CalledFrom()+txt+"\n", stuff...)
 		jWriteMsg("NOTICE", fmt.Sprintf(CalledFrom()+txt+"\n", stuff...))
 		if verboseSwitch == "on" {
@@ -45,16 +53,18 @@ func NoticeLog(txt string, stuff ...interface{}) {
 	}
 }
 
-// InfoLog sents text only to the infoLogger
+// InfoLog sends text only to the infoLogger
 func InfoLog(txt string, stuff ...interface{}) {
 	if infoLogger != nil {
+		infoLogger.SetPrefix(logTimeFormat()+severInfoFormat+logpidFormat)
 		infoLogger.Printf(CalledFrom()+txt+"\n", stuff...)
 	}
 }
 
-// WarningLog sents text to the warningLogger and stderr
+// WarningLog sends text to the warningLogger and stderr
 func WarningLog(txt string, stuff ...interface{}) {
 	if warningLogger != nil {
+		warningLogger.SetPrefix(logTimeFormat()+severWarnFormat+logpidFormat)
 		warningLogger.Printf(CalledFrom()+txt+"\n", stuff...)
 		jWriteMsg("WARNING", fmt.Sprintf(CalledFrom()+txt+"\n", stuff...))
 		if verboseSwitch == "on" {
@@ -63,17 +73,19 @@ func WarningLog(txt string, stuff ...interface{}) {
 	}
 }
 
-// ErrLog sents text only to the errorLogger
+// ErrLog sends text only to the errorLogger
 func ErrLog(txt string, stuff ...interface{}) {
 	if errorLogger != nil {
+		errorLogger.SetPrefix(logTimeFormat()+severErrorFormat+logpidFormat)
 		errorLogger.Printf(CalledFrom()+txt+"\n", stuff...)
 		jWriteMsg("ERROR", fmt.Sprintf(CalledFrom()+txt+"\n", stuff...))
 	}
 }
 
-// ErrorLog sents text to the errorLogger and stderr
+// ErrorLog sends text to the errorLogger and stderr
 func ErrorLog(txt string, stuff ...interface{}) error {
 	if errorLogger != nil {
+		errorLogger.SetPrefix(logTimeFormat()+severErrorFormat+logpidFormat)
 		errorLogger.Printf(CalledFrom()+txt+"\n", stuff...)
 		jWriteMsg("ERROR", fmt.Sprintf(CalledFrom()+txt+"\n", stuff...))
 		if errorSwitch == "on" {
@@ -87,9 +99,6 @@ func ErrorLog(txt string, stuff ...interface{}) error {
 func LogInit(logFile string, logSwitch map[string]string) {
 	var saptuneLog io.Writer
 
-	//define log format
-	logTimeFormat := time.Now().Format("2006-01-02 15:04:05.000 ")
-
 	if _, err := os.Stat(saptuneLogDir); err != nil {
 		if err = os.MkdirAll(saptuneLogDir, 0755); err != nil {
 			ErrorExit("", err)
@@ -101,15 +110,11 @@ func LogInit(logFile string, logSwitch map[string]string) {
 		ErrorExit("", err)
 	}
 
-	//saptuneWriter := io.MultiWriter(os.Stderr, saptuneLog)
-	//log.SetOutput(saptuneWriter)
-	//log.SetFlags(0)
-
-	debugLogger = log.New(saptuneLog, logTimeFormat+"DEBUG    "+logpidFormat, 0)
-	noticeLogger = log.New(saptuneLog, logTimeFormat+severNoticeFormat+logpidFormat, 0)
-	infoLogger = log.New(saptuneLog, logTimeFormat+severInfoFormat+logpidFormat, 0)
-	warningLogger = log.New(saptuneLog, logTimeFormat+severWarnFormat+logpidFormat, 0)
-	errorLogger = log.New(saptuneLog, logTimeFormat+severErrorFormat+logpidFormat, 0)
+	debugLogger = log.New(saptuneLog, "", 0)
+	noticeLogger = log.New(saptuneLog, "", 0)
+	infoLogger = log.New(saptuneLog, "", 0)
+	warningLogger = log.New(saptuneLog, "", 0)
+	errorLogger = log.New(saptuneLog, "", 0)
 
 	debugSwitch = logSwitch["debug"]
 	verboseSwitch = logSwitch["verbose"]
