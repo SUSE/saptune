ef64d19da9eac6792bfcad99fe9f3d8842b2e468
Wabri <12409541+Wabri@users.noreply.github.com>
2024-07-26 17:05:44 +0200

    feat(.github/workflows): update unit test pipeline and add release

---
+++ run_saptune_goreleaser.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "----------"
+echo "Building SAPTUNE binary in $bdate with version $Version"
+echo "----------"
+pwd
+ls -altr
+zypper -n --gpg-auto-import-keys install git
+git config --global --add safe.directory /app
+go mod init github.com/SUSE/saptune
+go mod tidy
+go install github.com/goreleaser/goreleaser@v1.24.0
+$(go env GOPATH)/bin/goreleaser release
